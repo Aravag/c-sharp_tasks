cd772bf04b9bce6386af69761bcfa91ec50cd6a4
Vagan Arakelyan <vra.vagan@gmail.com>
2022-11-21 12:42:51 +0100

    Done tasks 25, 27, 29

---
+++ task25/Program.cs
@@ -0,0 +1,10 @@
+﻿// Отрицательные числа тоже возводятся в степень
+Console.Write("Введите число, которое необходимо возвести в степень: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите степень: ");
+int numB = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+int count = 1;
+for(int i=1; i<=numB;i++){
+    count = count*numA;
+}
+Console.WriteLine($"Число {numA} в степени {numB} = {count}");
