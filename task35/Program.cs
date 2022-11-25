483f1fead178b6597993d9a22ee217f04aa93a6f
Vagan Arakelyan <vra.vagan@gmail.com>
2022-11-25 20:21:26 +0100

    Task 35 is done

---
+++ task35/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] FillArray(int size) {
+    int[] array = new int[size];
+    for(int i=0; i< array.Length; i++) {
+        array[i] = new Random().Next(0, 1000);
+    }
+    return array;
+}
+Console.Write("Введите длину массива: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] arr = FillArray(num);
+int count = 0;
+Console.WriteLine(string.Join(", ", arr));
+for(int i=0; i<arr.Length; i++) {
+    if(arr[i] > 10 && arr[i] < 99) {
+        count+=1;
+    }
+}
+Console.Write($"В данном массиве {count} чисел от 10 до 99.");
