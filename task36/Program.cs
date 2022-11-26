af4156e66928858d972805ab41cc82d97c255ee4
Vagan Arakelyan <vra.vagan@gmail.com>
2022-11-26 19:34:24 +0100

    Task 36 is done

---
+++ task36/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("-------------------------------------------------------------------------");
+Console.Write("Введите длину массива: ");
+int num = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+int[] array = new int[num];
+Random rand = new Random();
+int count = 0;
+for (int i = 0; i < array.Length; i++) {
+    array[i] = rand.Next(-20, 21);
+    Console.Write(array[i] + " ");
+}
+for(int i=1;i<array.Length; i+=2) {
+    count+=array[i];
+}
+Console.WriteLine();
+Console.WriteLine($"Сумма чисел, стоящих на нечетных позициях равняется {count}.");
+Console.WriteLine("-------------------------------------------------------------------------");
