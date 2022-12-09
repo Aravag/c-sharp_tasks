8a3d44a80c957babddf7033ede40e333487b12bd
Vagan Arakelyan <vra.vagan@gmail.com>
2022-12-09 13:52:58 +0100

    Task 62 is done

--- task62/Program.cs
+++ task62/Program.cs
@@ -1,26 +1,4 @@
-﻿// int row = 8;
-// int colum = 8;
-// int count = 1;
-// int[,] array = new int[row, colum];
-// for(int i=0; i<colum; i++) {
-//     array[0,i] = count;
-//     count++;
-// }
-// for(int i=0; i<row; i++) {
-//     array[i,colum-1] = count;
-//     count++;
-// }
-// for(int i=colum-1; i<colum-1; i--) {
-//     array[row-1,i] = count;
-//     count++;
-// }
-// for (int i = 0; i < array.GetLength(0); i++) {
-//     for (int j = 0; j < array.GetLength(1); j++) {
-//         Console.Write(array[i, j]);
-//     }
-//     Console.WriteLine();
-// }
-void PrintArray(int[,] a) {
+﻿void PrintArray(int[,] a) {
     for (int i = 0; i < a.GetLength(0); i++) {
         for (int j = 0; j < a.GetLength(1); j++) {
             if(a[i, j] < 10) {
