a593793fb1e7b7aa1d0e440f22cc7b350c9e40c0
Vagan Arakelyan <vra.vagan@gmail.com>
2022-12-09 16:04:14 +0100

    Task 58 is done

---
+++ task58/Program.cs
@@ -0,0 +1,39 @@
+﻿int row = 2;
+int colum = 2;
+int[,] arrayA = new int[row, colum];
+int[,] arrayB = new int[row, colum];
+void PrintArray(int[,] array) {
+    for(int i=0; i<row; i++) {
+        for(int j=0; j<colum; j++) {
+            Console.Write(array[i,j] + "\t ");
+        }
+        Console.WriteLine();
+    }
+}
+for(int i=0; i<row; i++) {
+    for(int j=0; j<colum; j++) {
+        arrayA[i,j] = new Random().Next(1,10);
+        arrayB[i,j] = new Random().Next(1,10);
+    }
+}
+Console.WriteLine("-----------------------------------------------------");
+Console.WriteLine("Первая матрица: ");
+Console.WriteLine("-----------------------------------------------------");
+PrintArray(arrayA);
+Console.WriteLine("-----------------------------------------------------");
+Console.WriteLine("Вторая матрица: ");
+Console.WriteLine("-----------------------------------------------------");
+PrintArray(arrayB);
+Console.WriteLine("-----------------------------------------------------");
+int[,] arrayC = new int[row, colum];
+for(int i=0; i<row; i++) {
+    for(int j=0; j<colum; j++) {
+        for(int k=0; k<colum; k++) {
+            arrayC[i,j] += arrayA[i,k]*arrayB[k,j];
+        }
+    }
+}
+Console.WriteLine("Произведение матриц: ");
+Console.WriteLine("-----------------------------------------------------");
+PrintArray(arrayC);
+Console.WriteLine("-----------------------------------------------------");
