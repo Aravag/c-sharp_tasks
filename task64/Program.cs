118ed73d4e6cf7b2c1d2a55f860bc7f08653f783
Vagan Arakelyan <vra.vagan@gmail.com>
2022-12-09 20:10:50 +0100

    Task 64 is done

---
+++ task64/Program.cs
@@ -0,0 +1,12 @@
+﻿void Numbers (int num) {
+    if(num == 0) {
+        return;
+    }
+    else {
+        Console.Write(num);
+        Numbers(num-1);
+    }
+}
+Console.Write("Введите число: ");
+int num = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+Numbers(num);
